166f7a996e74b51e8563109f182ec461593b21ac
zhouweifeng <86508962@qq.com>
2018-05-19 11:26:19 +0800

    删除多余文件

--- SpringBootTest/src/main/java/springboot/advice/ResponseAdvice.java
+++
@@ -1,6 +0,0 @@
-package springboot.advice;
-
-import org.springframework.web.servlet.mvc.method.annotation.JsonViewResponseBodyAdvice;
-
-public class ResponseAdvice extends JsonViewResponseBodyAdvice {
-}
